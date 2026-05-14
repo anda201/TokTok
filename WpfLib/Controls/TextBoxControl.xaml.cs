a8915e80c630e643b3f292413ba36ce6bb91e3f1
안다정 <dksekwjd312@naver.com>
2026-05-14 21:46:25 +0900

    [feat] TextBoxControl 사용자 정의 컨트롤 및 워터마크 기능 구현

---
+++ WpfLib/Controls/TextBoxControl.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLib.Controls
+{
+    /// <summary>
+    /// TextBoxControl.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class TextBoxControl : UserControl
+    {
+        public TextBoxControl()
+        {
+            InitializeComponent();
+        }
+
+        #region Public Properties
+        public bool Validating      
+        {
+            get { return (bool)GetValue(ValidatingProperty); }
+            set { SetValue(ValidatingProperty, value); }
+        }
+        public new Brush BorderBrush /*new를 통해서 기존의 상속 속성을 가린다.*/
+        {
+            get { return (Brush)GetValue(BorderBrushProperty); }
+            set { SetValue(BorderBrushProperty, value); }
+        }
+        public new Thickness BorderThickness
+        {
+            get { return (Thickness)GetValue(BorderThicknessProperty); }
+            set { SetValue(BorderThicknessProperty, value); }
+        }
+        public string WaterMarkText
+        {   
+            get { return (string)GetValue(WaterMarkTextProperty); }
+            set { SetValue(WaterMarkTextProperty, value); }
+        }
+        public Brush WaterMarkTextColor
+        {
+            get { return (Brush)GetValue(WaterMarkTextColorProperty); }
+            set { SetValue(WaterMarkTextColorProperty, value); }
+        }
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        #endregion
+
+        #region Public Statics
+        public static readonly DependencyProperty ValidatingProperty =
+            DependencyProperty.Register("Validating", typeof(bool), typeof(TextBoxControl), new PropertyMetadata(false));
+        public static readonly DependencyProperty BorderBrushProperty =
+            DependencyProperty.Register("BorderBrush", typeof(Brush), typeof(TextBoxControl), new UIPropertyMetadata(Brushes.SkyBlue));
+        public static readonly DependencyProperty BorderThicknessProperty =
+            DependencyProperty.Register("BorderThickness", typeof(Thickness), typeof(TextBoxControl), new UIPropertyMetadata(new Thickness(1)));
+        public static readonly DependencyProperty WaterMarkTextProperty =
+            DependencyProperty.Register("WaterMarkText", typeof(string), typeof(TextBoxControl), new PropertyMetadata(null));
+        public static readonly DependencyProperty WaterMarkTextColorProperty =
+            DependencyProperty.Register("WaterMarkTextColor", typeof(Brush), typeof(TextBoxControl), new PropertyMetadata(Brushes.Gray));
+        // 투웨이 방식으로 텍스트 속성을 바인딩
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(TextBoxControl), new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        #endregion
+
+    }
+}
---
+++ WpfLib/Converters/ValidatingBorderThicknessConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfLib.Converters
+{
+    public class ValidatingBorderThicknessConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool? isValidation = values[0] as bool?;
+            // 디자이너에서 null이 들어올 수 있기 때문에 null 체크를 해준다.
+            if (isValidation != null && isValidation == true)
+            {
+                return new Thickness(2);
+            }
+            else
+            {
+                return (Thickness)values[1];
+            }
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
